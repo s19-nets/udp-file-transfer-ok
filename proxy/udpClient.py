b149ad744e4c0116e79fd2d14746107555197a17
root <root@localhost.localdomain>
2019-02-15 00:23:46 -0600

    file transfer working

--- proxy/udpClient.py
+++ proxy/udpClient.py
@@ -2,7 +2,7 @@
 from socket import *
 
 # default params
-serverAddr = ('localhost', 50000)       
+serverAddr = ('localhost', 50001)       
 
 import sys, re                          
 
@@ -26,7 +26,77 @@ except:
 
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Input lowercase msg:")
-clientSocket.sendto(message.encode(), serverAddr)
-modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
-print("Modified message from %s is <%s>" % (repr(serverAddrPort), modifiedMessage))
+#print("Input lowercase msg")
+usrInput =''
+while usrInput is not 'q':
+    # Asking user for input file and put or get command
+    print("Entering 'put' and a file name will allow you to transfer a file:")
+    usrInput = sys.stdin.readline()[:-1]     # delete final \n
+    # Splitting input by space to check if put command and filename is present
+    splitInput = usrInput.split(' ')
+    if splitInput[0].strip() == 'put':
+        usrFileName = splitInput[-1]
+        # Try to open file, if not, print that file not found and loop again for input
+        try:
+            fileToSend = open(usrFileName, 'rb')
+
+            # Starting message, to pass in file name and start message (header)
+            clientSocket.sendto(b'start ' + usrFileName.encode(), serverAddr)
+
+            # Opening the file as a binary, to be able to send 1000 bytes at a time
+            with open(usrFileName.strip(), 'rb') as binary_file:
+                # Variable to grab bytes from the file for sending
+                byteData = binary_file.read()
+
+            # Replacing new lines with special character to avoid errors and replace them back later
+            byteData = byteData.replace(b'\n',b'~`')
+
+            # Checking if the length of the byteData is 1000 bytes or more, if so, send the data
+            while len(byteData) >= 1000:
+                # Send variable for the beginning 1000 bytes
+                send = byteData[:1000]
+
+                # Move the byteData from the last send 1000 bytes to the next 1000 bytes, or end of the byteData
+                byteData = byteData[1000:]
+
+                # Try to send the 1000 bytes of byteData (send)
+                try:
+                    clientSocket.sendto(send, serverAddr)
+                except:
+                    print("Broken pipe, exiting")
+                    sys.exit(1)
+
+            # If byteData is still greater than 0, send the remaining bytes that were not apart of the last 1000 bytes
+            if len(byteData) > 0:
+                clientSocket.sendto(byteData, serverAddr)
+            
+            # Sending the end signal to know that the file is done sending
+            clientSocket.sendto(b"~fInIs", serverAddr)
+            
+            # Receiving the file received success message
+            recMessage, serverAddrPort = clientSocket.recvfrom(2048)
+            if recMessage:
+                print("received %s from %s:" % (recMessage, repr(serverAddrPort)))
+                sys.exit(0)
+
+        # Match enclosing try
+        except FileNotFoundError:
+            print("Wrong file or file path")
+            continue
+        #print(sending file %s" %s (usrFileName))
+
+    # Else for enclosing userInput if statement
+    elif usrInput.strip() == 'q':
+        print("Exiting")
+        sys.exit(0)
+
+    else:
+        print("Invalid please try again, enter 'q' to exit")
+
+
+#print("Input lowercase msg")
+#message = sys.stdin.readline()[:-1]     # delete final \n
+#message = input("Input lowercase msg:")
+#clientSocket.sendto(message.encode(), serverAddr)
+#modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
+#print("Modified message from %s is <%s>" % (repr(serverAddrPort), modifiedMessage))
--- proxy/udpServer.py
+++ proxy/udpServer.py
@@ -4,7 +4,7 @@ from socket import *
 # default params
 serverAddr = ("", 50001)
 
-import sys
+import sys, os
 def usage():
     print("usage: %s [--serverPort <port>]"  % sys.argv[0])
     sys.exit(1)
@@ -27,9 +27,63 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(serverAddr)
 print("ready to receive")
 while 1:
-    message, clientAddrPort = serverSocket.recvfrom(2048)
-    print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddrPort)
+    #message, clientAddrPort = serverSocket.recvfrom(2048)
+    headerPayload, clientAddrPort = serverSocket.recvfrom(2048)
+    print("from %s: rec'd connection" % (repr(clientAddrPort)))
+    
+    # Getting current working directory to create a new directory for server files
+    cwd = os.getcwd()
+
+    # Initializing fileName variable
+    fileName = ''
+
+    # While loop that waits for fileName to get initiated
+    while fileName == '':
+        # Recieve filename and save it as "header"
+        #headerPayload = framedReceive(serverSocket)
+        # If headerPayload is not NON
+        if headerPayload:
+            # Split the decoded payload by the spaces
+            pl = headerPayload.decode().split()
+        # If start is in the payload by the spaces
+        if b'start' in headerPayload:
+            fileName = pl[-1]
+            # If serverDirectory does not exist, create it as a subdirectory
+            if not os.path.exists(cwd + '/serverDirectory/'):
+                os.makedirs(cwd + '/serverDirectory')
+            # Open the file in serverDirectory (creating it if it does not exist)
+
+            fileOpen = open(os.path.join(cwd + '/serverDirectory/', fileName), 'wb+')
+            fileOpen.close()
+            break
+        # While loop for receiving the body of the file and writing it to the one created on the server
+    while True:
+        payload, port = serverSocket.recvfrom(2048)
+        #print("payload is: %s " % (payload.decode()))
+        if not payload:
+            sys.exit(0)
+        # Replacing the '~`" back to '\n' new line character
+        payload = payload.decode().replace('~`', '\n')
+        # Opening the file for appending
+        #currFile = cwd + '/serverDirectory/' 
+        fileOpen = open(cwd + '/serverDirectory/' + fileName, 'a')
+        try:
+            # If the finish character is found in the payload to show file is done sending
+            if '~fInIs' in payload:
+                fileOpen.close()
+                success = b"File finished sending"
+                #print(success)
+                # Send that the file was received successully
+                serverSocket.sendto(success, clientAddrPort)
+                sys.exit(0)
+            # If it is not finished, write to the file
+            else:
+                fileOpen.write(payload)
+        except FileNotFoundError:
+            print("Error trying to receive file")
+
+    #message = sys.stdin.readline()[:-1]     # delete final \n
+    #modifiedMessage = message.upper()
+    #serverSocket.sendto(modifiedMessage, clientAddrPort)
     
                 
